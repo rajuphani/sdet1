764e7fd41c940aa7e3a9b348e361c9d9cc633cd8
rajuphani <tatavarthiraju@gmail.com>
2024-01-09 20:41:34 -0500

    Update Test1 in main branch.java

--- src/test/java/packagetest1/Test1.java
+++ src/test/java/packagetest1/Test1.java
@@ -3,5 +3,7 @@ package src.test.java.packagetest1;
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("This is my first class");
+         System.out.println("This is my second step");
+        
     }
 }
